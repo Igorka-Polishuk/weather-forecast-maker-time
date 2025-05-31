06e9720ed0ab915bc93bf1b1f135f88b9534c6cb
Igorka-Polishuk <igor1990polisukk@gmail.com>
2025-05-31 10:29:39 +0300

    Lesson 1

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,13 +1,1 @@
-const socket = io();
 
-const button = document.querySelector('button');
-
-button.addEventListener('click', () => {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Lviv&appid=111e2769d1897b030167fee7eb6d5152&units=metric&lang=ua')
-        .then(async result => await result.json())
-        .then(result => {
-            console.log(result);
-        });
-});
-
-socket.on('test_event', message => alert(message));
