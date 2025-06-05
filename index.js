10cb68a01c74f46fee1e4f1311dce5a5ce1a54e2
Igorka-Polishuk <igor1990polisukk@gmail.com>
2025-06-05 18:54:39 +0300

    Lesson 2

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
-const https = require('https');
-const http = require('http');
+require('dotenv').config();
 
-const PORT = 3000;
+const http = require('http');
+const https = require('https');
+const { Server } = require('socket.io');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3000;
+const API_KEY = process.env.API_KEY;
+const CURRENT_WEATHER_API = process.env.CURRENT_WEATHER_API;
+const FUTURE_WEATHER_API = process.env.FUTURE_WEATHER_API;
+const CITY_COORDINATES_GETTING_API = process.env.CITY_COORDINATES_GETTING_API;
 
 const server = http.createServer((request, response) => {
-    if (request.method === 'GET') {
-        if (request.url === '/api/weather') {
-            https.get('https://api.openweathermap.org/data/2.5/weather?q=Lviv&appid=111e2769d1897b030167fee7eb6d5152&units=metric&lang=ua', result => {
-                let finalData = '';
-
-                result.on('data', data => { finalData += data; });
-                result.on('end', () => {
-                    response.writeHead(200, {'content-type': 'application/json'});
-                    response.end(finalData);
-                })
-                request.on('error', () => {
-                    response.writeHead(200, {'content-type': 'application/json'});
-                    response.end(JSON.stringify({message: "error"}));
-                });
-            });
-        }
+    if (request.url === '/') {
+        const context = fs.readFileSync(path.resolve('src', 'static', 'index.html'));
+        response.writeHead(200, {'content-type': 'text/html'});
+        response.end(context.toString());
+    } else if (request.url.includes('root.css')) {
+        const context = fs.readFileSync(path.resolve('src', 'static', 'css', 'root.css'));
+        response.writeHead(200, {'content-type': 'text/css'});
+        response.end(context.toString());
+    }
+    else if (request.url.includes('index.css')) {
+        const context = fs.readFileSync(path.resolve('src', 'static', 'css', 'index.css'));
+        response.writeHead(200, {'content-type': 'text/css'});
+        response.end(context.toString());
+    } else if (request.url.includes('script.js')) {
+        const context = fs.readFileSync(path.resolve('src', 'static', 'js', 'script.js'));
+        response.writeHead(200, {'content-type': 'application/javascript'});
+        response.end(context.toString());
     }
 })
     .listen(PORT, () => console.log(`http://localhost:${PORT}`));
+
+const io = new Server(server);
+io.on('connection', socket => {
+    socket.on('get_weather_forecast', city => {
+        https.get(CURRENT_WEATHER_API + `?q=${city}&appid=${API_KEY}&units=metric&lang=ua`, answer => {
+            let data = '';
+
+            answer.on('data', chunk => { data += chunk; });
+            answer.on('end', () => {
+                const response = JSON.parse(data);
+                socket.emit('access_weather_forecast', response);
+            });
+        });
+    });
+});
--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,13 +1,43 @@
 const socket = io();
 
-const button = document.querySelector('button');
+const weatherInfoBlock = document.querySelector('.weather__info');
+const cityHeaderForm = document.querySelector('.header__form');
 
-button.addEventListener('click', () => {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Lviv&appid=111e2769d1897b030167fee7eb6d5152&units=metric&lang=ua')
-        .then(async result => await result.json())
-        .then(result => {
-            console.log(result);
-        });
+cityHeaderForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const city = event.target['city'].value;
+
+    if (!city) {
+        alert('Field not to has to be empty!');
+        return;
+    }
+
+    socket.emit('get_weather_forecast', city);
+    socket.on('access_weather_forecast', response => {
+        weatherInfoBlock.innerHTML = `
+            <div class="weather__info__city">
+                    <h2>${city}</h2>
+                </div>
+                <div class="weather__info__temperature">${response.main.temp}°С</div>
+                <div class="weather__info__addiction">
+                    <div class="weather__info__addiction__block">
+                        <p class="addiction__block__property__name">Відчувається як</p>
+                        <p class="addiction__block__property__value">${response.main.feels_like}°С</p>
+                    </div>
+                    <div class="weather__info__addiction__block">
+                        <p class="addiction__block__property__name">Вологість</p>
+                        <p class="addiction__block__property__value">${response.main.humidity}%</p>
+                    </div>
+                    <div class="weather__info__addiction__block">
+                        <p class="addiction__block__property__name">Швидкість вітру</p>
+                        <p class="addiction__block__property__value">${response.wind.speed} км/год</p>
+                    </div>
+                    <div class="weather__info__addiction__block">
+                        <p class="addiction__block__property__name">Погода</p>
+                        <p class="addiction__block__property__value">${response.weather[0].description}</p>
+                    </div>
+                </div>
+        `;
+    });
 });
-
-socket.on('test_event', message => alert(message));
