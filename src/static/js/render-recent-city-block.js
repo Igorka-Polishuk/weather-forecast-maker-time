4c9b33630cea81261f4f62965131683345d90afc
Igorka-Polishuk <igor1990polisukk@gmail.com>
2025-06-25 10:38:31 +0300

    Lesson 4(Final version)

--- src/static/js/render-recent-city-block.js
+++ src/static/js/render-recent-city-block.js
@@ -1,13 +1,16 @@
 const selectRecentCity = document.querySelector("#select-recent-city");
 
 function renderRecentCityBlock(cities) {
+    const beginContext = `
+        <option value="" disabled selected>Ваші недавні міста</option>
+    `;
     let finalContext = `
         <option value="" disabled selected>Ваші недавні міста</option>
         <option value="delete_all">Видалити всі</option>
     `;
 
     if (!cities) {
-        selectRecentCity.innerHTML = finalContext;
+        selectRecentCity.innerHTML = beginContext;
         return;
     }
 
