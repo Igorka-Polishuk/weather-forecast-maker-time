e75c8ea4d50a4aea1f36254349f343ee94d8ee10
Igorka-Polishuk <igor1990polisukk@gmail.com>
2025-06-25 10:28:08 +0300

    Begin

--- index.js
+++ index.js
@@ -1,59 +1,31 @@
-require('dotenv').config();
-
 const { createServer } = require('http');
 const { Server } = require('socket.io');
 
 const { processRequest } = require('./src/modules/process-requests.js');
 const { makeGetRequest } = require('./src/modules/make-https-requests.js');
-const { SQLiteSystem } = require('./src/modules/process-database-requests.js');
 
-const PORT = process.env.PORT || 3000;
-const API_KEY = process.env.API_KEY;
-const CURRENT_WEATHER_API = process.env.CURRENT_WEATHER_API;
-const FUTURE_WEATHER_API = process.env.FUTURE_WEATHER_API;
+const PORT = 4000;
 
 const server = createServer((request, response) => {
-    processRequest(request.url, response);
+    return processRequest(request, response);
 })
     .listen(PORT, () => console.log(`http://localhost:${PORT}`));
 
 const io = new Server(server);
 io.on('connection', async socket => {
-     const databaseController = new SQLiteSystem();
+    socket.on('get_weather_forecast', async city => {
+        await makeGetRequest(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=111e2769d1897b030167fee7eb6d5152&units=metric&lang=ua`)
+            .then(async answer => {
+                socket.emit('access_weather_forecast', { city, ...answer });
 
-    try {
-        await databaseController.awaitForDatabase();
-
-        socket.on('get_weather_forecast', async city => {
-            await makeGetRequest(CURRENT_WEATHER_API + `?q=${city}&appid=${API_KEY}&units=metric&lang=ua`)
-                .then(async answer => {
-                    socket.emit('access_weather_forecast', { city, ...answer });
-
-                    await makeGetRequest(
-                        `${FUTURE_WEATHER_API}?lat=${answer.coord.lat}&lon=${answer.coord.lon}&exclude=current,minutely,daily,alerts&appid=${API_KEY}&units=metric&lang=ua`
-                    ).then(futureWeather => {
-                        socket.emit('access_future_weather', futureWeather);
-                    });
-                })
-                .catch(error => {
-                    socket.emit('error_access_weather_forecast', error);
+                await makeGetRequest(
+                    `https://api.openweathermap.org/data/2.5/forecast?lat=${answer.coord.lat}&lon=${answer.coord.lon}&exclude=current,minutely,daily,alerts&appid=111e2769d1897b030167fee7eb6d5152&units=metric&lang=ua`
+                ).then(futureWeather => {
+                    socket.emit('access_future_weather', futureWeather);
                 });
-        });
-
-        socket.on('add_new_user', async user => {
-            try {
-                const result = await databaseController.addNewUser(user);
-
-                socket.emit('add_new_user_result', result);
-            } catch (error) {
-                socket.emit('add_new_user_result', error);
-            }
-        });
-
-        socket.on('disconnect', async () => {
-            await databaseController.closeDatabase();
-        });
-    } catch (error) {
-        console.error(error.message);
-    }
+            })
+            .catch(error => {
+                socket.emit('error_access_weather_forecast', error);
+            });
+    });
 });
--- src/modules/process-requests.js
+++ src/modules/process-requests.js
@@ -3,33 +3,29 @@ const { readFileSync, existsSync } = require('fs');
 
 const gettingFileName = require('./get-file-name-from-url.js');
 
-function processRequest(url, response) {
-    if (url === '/') {
+async function processRequest(request, response) {
+    if (request.url === '/' && request.method === 'GET') {
         const filePath = resolve('src', 'static', 'index.html');
         const fileContext = readFileSync(filePath).toString();
 
         response.writeHead(200, { 'content-type': 'text/html' });
         return response.end(fileContext);
-    } else {
-        if (existsSync(resolve('src', 'static', 'css', gettingFileName.getCSSFileName(url)))) {
-            const filePath = resolve('src', 'static', 'css', gettingFileName.getCSSFileName(url));
-            const fileContext = readFileSync(filePath).toString();
+    }
 
-            response.writeHead(201, {'content-type': 'text/css'});
-            return response.end(fileContext);
-        } else if (existsSync(resolve('src', 'static', 'js', gettingFileName.getJSFilename(url)))) {
-            const filePath = resolve('src', 'static', 'js', gettingFileName.getJSFilename(url));
-            const fileContext = readFileSync(filePath).toString();
+    if (existsSync(resolve('src', 'static', 'css', gettingFileName.getCSSFileName(request.url)))) {
+        const filePath = resolve('src', 'static', 'css', gettingFileName.getCSSFileName(request.url));
+        const fileContext = readFileSync(filePath).toString();
 
-            response.writeHead(201, {'content-type': 'application/javascript'});
-            return response.end(fileContext);
-        } else if (existsSync(resolve('src', 'static', 'html', gettingFileName.getHTMLFileName(url)))) {
-            const filePath = resolve('src', 'static', 'html', gettingFileName.getHTMLFileName(url));
-            const fileContext = readFileSync(filePath).toString();
+        response.writeHead(201, { 'content-type': 'text/css' });
+        return response.end(fileContext);
+    }
 
-            response.writeHead(201, {'content-type': 'text/html'});
-            return response.end(fileContext);
-        }
+    if (existsSync(resolve('src', 'static', 'js', gettingFileName.getJSFilename(request.url)))) {
+        const filePath = resolve('src', 'static', 'js', gettingFileName.getJSFilename(request.url));
+        const fileContext = readFileSync(filePath).toString();
+
+        response.writeHead(201, { 'content-type': 'application/javascript' });
+        return response.end(fileContext);
     }
 }
 
